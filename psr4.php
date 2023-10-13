45f7bec92abd8d17d3afabeb3e9d337ce6fbf9b1
Davide Colombo <2599183+davidecolombo@users.noreply.github.com>
2023-10-13 21:18:43 +0200

    Create psr4.php

---
+++ psr4.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace SpotifyWebAPI;
+
+spl_autoload_register(function($class) {
+  if (stripos($class, __NAMESPACE__) === 0) {
+    @include(__DIR__ . DIRECTORY_SEPARATOR . 'src' . str_replace('\\', DIRECTORY_SEPARATOR, substr($class, strlen(__NAMESPACE__))) . '.php');
+  }
+});
